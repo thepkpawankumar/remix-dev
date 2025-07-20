32f5e7abf7dbfcfe4e9a87c15dc12ff3cf403f60
Pawan <thepkpawankumar@gmail.com>
2025-07-20 13:27:09 +0530

    - Setup remix using vite - Started implementing tailwind css - Explored folder structure - Started a basic contact registory application

---
+++ remix-app/app/routes/about.tsx
@@ -0,0 +1,9 @@
+// We can't cmobile export default with const directly, it is an invalid javascript syntax
+const About = () => {
+    return (
+        <div>
+            <h1>About us root</h1>
+        </div>
+    )
+}
+export default About;
